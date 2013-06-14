2974cd8e9e16b2052498a451b1259a635060ea32
St George's Healthcare NHS Trust <communications@stgeorges.nhs.uk>
2013-06-14 15:54:12 +0100

    Adding CSS, PHP

--- sghpress/searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div><label class="screen-reader-text hiddentext" for="s">Search for:</label>
-        <input type="text" value="" name="s" id="s" class="searchbox twelvecol" />
-        <!-- <input type="submit" id="searchsubmit" value="Search" /> -->
-    </div>
-</form>
