1132324f38554a4fd6a72bb77734d6eef835c0c9
Luke Oatham <lukeoatham@gmail.com>
2013-07-18 15:38:08 +0100

    Include custom post types in tag listing
    Signed-off-by: Luke Oatham <lukeoatham@gmail.com>

--- sghpress/functions.php
+++ sghpress/functions.php
@@ -975,3 +975,18 @@ function ht_connection_types() {
 }
 add_action( 'wp_loaded', 'ht_connection_types' );
 
+function wpse28145_add_custom_types( $query ) {
+    if( is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+
+        // this gets all post types:
+        $post_types = get_post_types();
+
+        // alternately, you can add just specific post types using this line instead of the above:
+        // $post_types = array( 'post', 'your_custom_type' );
+
+
+        $query->set( 'post_type', $post_types );
+        return $query;
+    }
+}
+add_filter( 'pre_get_posts', 'wpse28145_add_custom_types' );
