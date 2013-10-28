5632b49c000452b0bc5149bb46fe2fffe534c85c
Colin Wren <colin@gimpneek.com>
2013-10-28 23:38:09 +0000

    Making search form aria compliant

--- sghpress/searchform.php
+++ sghpress/searchform.php
@@ -1,11 +1,11 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div class="input-prepend input-append">
-    	<label class="screen-reader-text hidden-phone add-on" for="s">Search:</label>
+    	<label class="screen-reader-text hidden-phone add-on" id="searchLabel" for="s">Search:</label>
         <input type="text" value="<?php
         if(is_search()){
 	        echo $_GET["s"];
         }
-         ?>" name="s" id="appendedPrependedInput" class="searchbox twelvecol"  />
+         ?>" name="s" id="appendedPrependedInput" class="searchbox twelvecol" role="search" aria-labelledby="searchLabel" />
          <button class="btn searchButton" type="submit">
 	         <script type="text/javascript">
 				var imageURL = "<?php echo get_stylesheet_directory_uri(); ?>/images/search";
